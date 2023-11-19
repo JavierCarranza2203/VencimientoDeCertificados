1bc01cb45a96edaaafec619f9be10ab53f7cb401
Javier Carranza <137971245+JavierCarranza2203@users.noreply.github.com>
2023-11-19 13:25:52 -0600

    Delete CarpetaPruebas directory

--- CarpetaPruebas/prueba.js
+++
@@ -1,7 +0,0 @@
-function PeticionHTTP() {
-    fetch("https://localhost/VerificadorDeVencimientoDeFirmas/App/Views/prueba.php")
-        .then(response => response.json())
-            .then(datosJson => console.log(datosJson));
-}
-
-PeticionHTTP();
